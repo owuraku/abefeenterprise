485066b502b48e2e9cf72395b4c632fe2a9a8768
Seth Gyekye-Boateng <boatseth3@gmail.com>
2022-03-16 09:41:56 +0000

    changed layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { RouterModule } from '@angular/router';
 import { MenubarModule } from 'primeng/menubar';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @NgModule({
   declarations: [LayoutComponent],
   exports: [LayoutComponent],
-  imports: [CommonModule, RouterModule, MenubarModule],
+  imports: [CommonModule, RouterModule, MenubarModule, StyleClassModule],
 })
 export class LayoutModule {}
