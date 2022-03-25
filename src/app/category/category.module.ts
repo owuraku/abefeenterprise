36c1fb46ecbfa65ed84056c561f36e5e436cde7d
kuffour <eawitikuffour.k@gmail.com>
2022-03-25 08:21:31 +0000

    category ui added

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryComponent } from './category.component';
 import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
-// import { MessageService } from 'primeng/api';
 import { InputTextModule } from 'primeng/inputtext';
-
+import { DialogModule } from 'primeng/dialog';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [CategoryComponent],
   imports: [
@@ -16,10 +15,10 @@ import { SplitButtonModule } from 'primeng/splitbutton';
     CategoryRoutingModule,
     CardModule,
     TableModule,
-    // MessageService,
     InputTextModule,
     SplitButtonModule,
+    DialogModule,
+    FormsModule,
   ],
-  // providers: [{ provide: MessageService }],
 })
 export class CategoryModule {}
