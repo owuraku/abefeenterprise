032d70a41f792656213aed5cb02c2e8368e4d9a0
kuffour <eawitikuffour.k@gmail.com>
2022-04-19 10:59:26 +0000

    Add and Edit Product Implementation

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,30 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../service/product.service';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ProductDetails } from './product.model';
+import { CategoryService } from '../service/category.service';
 
-export interface Product {
-  name: string;
-  price: number;
-  category: string;
-}
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
-  product: Product[] = [];
+  hideContent: number | undefined;
+
+  product: ProductDetails[] = [];
+  _product!: ProductDetails;
 
   displayBasic: boolean = false;
+  editDialog = false;
+  selectedCategory: any[] = [];
+  filtereredCategoryArray: any[] = [];
+  category: any[] = [];
 
-  showBasicDialog() {
+  showAddProductDialog() {
+    this.hideContent = 1;
     this.displayBasic = true;
   }
 
-  constructor() {}
+  showEditProductDialog(product: ProductDetails) {
+    this._product = { ...product };
+    this.productForm.patchValue(product);
+    this.displayBasic = true;
+    this.hideContent = 0;
+  }
+
+  productForm = new FormGroup({
+    name: new FormControl(''),
+    category: new FormControl(''),
+    sellingPrice: new FormControl(''),
+    costPrice: new FormControl(''),
+    currentStock: new FormControl(''),
+    status: new FormControl(true),
+  });
+
+  columnDef = [
+    { field: 'name', header: 'Name' },
+    { field: 'category', header: 'Category' },
+    { field: 'Selling Price', header: 'Selling Price' },
+    { field: 'Cost Price', header: 'Cost Price' },
+    { field: 'Current Stock', header: 'Current Stock' },
+    { field: 'status', header: 'Active' },
+  ];
+  constructor(
+    private productService: ProductService,
+    private cdref: ChangeDetectorRef,
+    private categoryService: CategoryService
+  ) {}
 
   ngOnInit() {
-    this.product = [
-      { name: 'Milo', price: 10, category: 'Provision' },
-      { name: 'Chicken', price: 20, category: 'Coldstore' },
-    ];
+    this.productService.getProduct().subscribe((res: any) => {
+      this.product = [...res];
+      this.cdref.detectChanges();
+      console.log(res);
+    });
+    this.categoryService.getCategory().subscribe((result: any) => {
+      this.category = [...result];
+      console.log(this.category);
+    });
+  }
+  addProduct() {
+    const data = this.productForm.value;
+    data.category = this.category[0].id;
+    this.productService.addProduct(data).subscribe((res) => console.log(res));
+  }
+
+  saveEditProduct(id: string) {
+    const data = {
+      name: this.productForm.value.name,
+      category: this.productForm.value.category.id,
+      sellingPrice: this.productForm.value.sellingPrice,
+      costPrice: this.productForm.value.costPrice,
+      status: true,
+    };
+    this.productService.updateProduct(id, data).subscribe((res: any) => {
+      console.log(data);
+    });
+  }
+  filterCategory(event: any) {
+    let filtered: any[] = [];
+    let query = event.query;
+    for (let i = 0; i < this.category.length; i++) {
+      let cat = this.category[i];
+      if (cat.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+        filtered.push(cat);
+      }
+    }
+
+    this.filtereredCategoryArray = filtered;
   }
 }
